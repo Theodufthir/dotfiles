153f97f048b3132d18a13afb6fbe1e04809bb364
Théo Dufour <theo.dufthir@gmail.com>
2025-01-06 18:59:21 +0100

    feat(astal): updated media button to account for "no player" situation

--- widgets/bar/media.tsx
+++ widgets/bar/media.tsx
@@ -6,20 +6,18 @@ import { toggleOnCurrentMonitor } from "../../utils/monitors";
 
 const mpris = Mpris.get_default()
 
-function Media() {
+const Media = () => {
   const mainPlayer = nBind(mpris, "players").as(ps => ps[0])
-  const hasMedia = nBind(mainPlayer, "entry").as(e => e !== null)
-  const text = nDerive(mainPlayer, ["entry", "title", "artist"],
-    ({ entry, title, artist }) => entry ? `${title} - ${artist}` : "No media"
-  )
+  const hasMedia = nBind(mainPlayer, "entry").as(e => e !== null && e !== undefined)
+  const text = nDerive(mainPlayer, ["title", "artist"], ({ title, artist }) => `${title} - ${artist}`)
 
   return <Button
-    className={hasMedia.as(e => `media ${e !== null ? "highlightable" : "no-media"}`)}
+    className={hasMedia.as(e => `media ${e ? "highlightable" : "no-media"}`)}
     sensitive={hasMedia}
     onPrimaryClick={() => toggleOnCurrentMonitor(MediaPopup)}
     onSecondaryClick={() => mainPlayer.get().play_pause()}
     onDestroy={() => text.drop()}>
-    {nBind(text)}
+    {nBind(text).as(t => t ?? "No media")}
   </Button>
 }
 
