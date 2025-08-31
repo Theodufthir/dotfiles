fd641ce2693171f8a634fb83e00f1708fa142b79
Théo Dufour <theo.dufthir@gmail.com>
2025-08-31 21:51:42 +0200

    feat(nix): isolated systems function in file, added hardware config file in said function

---
+++ hosts/common.nix
@@ -0,0 +1,47 @@
+{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
+  nixosSystem = {
+    folder, system,
+    useUnstable ? true,
+    specialArgs ? {},
+    hasHomeManager ? true,
+    modules ? [],
+    overlays ? []
+  }:
+  (if useUnstable then nixpkgs-unstable else nixpkgs).lib.nixosSystem {
+    inherit system;
+
+    specialArgs = {
+      hm-host-config = if !hasHomeManager then {} else import (./. + "/${folder}/home-manager.nix");
+    } // (if !useUnstable then {} else {
+      pkgs-stable-25-05 = import nixpkgs {
+        inherit system overlays;
+        config.allowUnfree = true;
+      };
+    }) // specialArgs;
+
+    modules = modules ++ [
+      (./. + "/${folder}/configuration.nix")
+      (./. + "/${folder}/drivers.nix")
+      (./. + "/${folder}/hardware-configuration.nix")
+      {
+        nixpkgs.overlays = overlays;
+        nixpkgs.config.allowUnfree = true;
+        nix.settings = {
+          experimental-features = [ "nix-command" "flakes" ];
+          extra-substituters = [
+            "https://nix-community.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+        };
+      }
+    ] ++ (if !hasHomeManager then [] else [
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+      }
+    ]);
+  };
+}
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,51 +1,5 @@
-{ self, nixpkgs, nixpkgs-unstable, home-manager, astal-bar, ... }@inputs:
-let
-  nixosSystem = {
-    folder, system,
-    useUnstable ? true,
-    specialArgs ? {},
-    hasHomeManager ? true,
-    modules ? [],
-    overlays ? []
-  }:
-  (if useUnstable then nixpkgs-unstable else nixpkgs).lib.nixosSystem {
-    inherit system;
-
-    specialArgs = {
-      hm-host-config = if !hasHomeManager then {} else import (./. + "/${folder}/home-manager.nix");
-    } // (if !useUnstable then {} else {
-      pkgs-stable-25-05 = import nixpkgs {
-        inherit system overlays;
-        config.allowUnfree = true;
-      };
-    }) // specialArgs;
-
-    modules = modules ++ [
-      (./. + "/${folder}/configuration.nix")
-      (./. + "/${folder}/drivers.nix")
-      {
-        nixpkgs.overlays = overlays;
-        nixpkgs.config.allowUnfree = true;
-      }
-      {
-        nix.settings = {
-          extra-substituters = [
-            "https://nix-community.cachix.org"
-          ];
-          extra-trusted-public-keys = [
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          ];
-        };
-      }
-    ] ++ (if !hasHomeManager then [] else [ 
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      }
-    ]);
-  };
-in {
+{ self, fprintd-55b4, astal-bar, ... }@inputs:
+with (import ./common.nix inputs); {
   yoga = nixosSystem rec {
     folder = "yoga";
     system = "x86_64-linux";
@@ -54,7 +8,7 @@ in {
     };
     overlays = [
       (final: prev: {
-        fprintd = inputs.fprintd-55b4.packages.${system}.fprintd;
+        fprintd = fprintd-55b4.packages.${system}.fprintd;
       })
     ];
     modules = [
